b945d2dea1764030b7692cef8df3bb1a53ef708e
chris <cf751653@sju.edu>
2023-10-25 18:49:03 -0400

    vehicles (not recent-trips)

--- app/main.py
+++ app/main.py
@@ -8,3 +8,4 @@ app.include_router(users.router, prefix="/users", tags=["Users"])
 app.include_router(trips.router, prefix="/trips", tags=["Trips"])
 app.include_router(campus_locations.router, prefix="/campus_locations", tags=["Campus Locations"])
 app.include_router(vehicles.router, prefix="/vehicles", tags=["Vehicles"])
+
