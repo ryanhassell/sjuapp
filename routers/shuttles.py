04a88292c9582b4a1202378d7e63f438bce99476
erinzahner <ez749184@sju.edu>
2023-11-20 19:38:53 -0500

    added shuttle type and status

--- routers/shuttles.py
+++ routers/shuttles.py
@@ -84,3 +84,29 @@ async def update_shuttle(shuttle_id: int, shuttle_data: ShuttleUpdate, db: Sessi
         return shuttle_to_update
     else:
         raise HTTPException(status_code=404, detail=f"Shuttle with ID {shuttle_id} not found")
+
+@router.get("/{shuttle_id}/status", response_model=dict)
+async def get_shuttle_status(shuttle_id: int, db: Session = Depends(get_db)):
+    shuttle = db.query(Shuttle).filter(Shuttle.id == shuttle_id).first()
+    if shuttle is None:
+        raise HTTPException(status_code=404, detail="Shuttle not found")
+
+    shuttle_status = {
+        "shuttle_id": shuttle.id,
+        "status": shuttle.shuttle_status
+    }
+    return shuttle_status
+
+@router.get("/{shuttle_id}/type", response_model=dict)
+async def get_shuttle_type(shuttle_id: int, db: Session = Depends(get_db)):
+    shuttle = db.query(Shuttle).filter(Shuttle.id == shuttle_id).first()
+    if shuttle is None:
+        raise HTTPException(status_code=404, detail="Shuttle not found")
+
+    shuttle_type = {
+        "shuttle_id": shuttle.id,
+        "type": shuttle.shuttle_type
+    }
+    return shuttle_type
+
+
