81264222f2741a14ab9289b6554baf8e97120fb0
chris <cf751653@sju.edu>
2023-12-04 18:29:31 -0500

    updated UI for this page

--- lib/driver_page.dart
+++ lib/driver_page.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
-import 'pickup_ride_page.dart'; // Make sure to import your PickupRidePage
+import 'pickup_ride_page.dart';
 
 class DriverPage extends StatelessWidget {
   Widget _buildMainButton({required String label, required VoidCallback onPressed}) {
     return ElevatedButton(
       onPressed: onPressed,
       child: Text(label),
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Colors.red[700], // Button background color
+        foregroundColor: Colors.white, // Text and icon color
+        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+        textStyle: const TextStyle(fontSize: 18),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30),
+        ),
+      ),
     );
   }
 
@@ -13,17 +22,19 @@ class DriverPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.red[900],
         title: Text('Driver Page'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Center(
+        child: Align(
+          alignment: Alignment.topCenter,
           child: _buildMainButton(
             label: 'Current Ride Requests',
             onPressed: () {
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => PickupRidePage()), // Replace with your PickupRidePage
+                MaterialPageRoute(builder: (context) => PickupRidePage()),
               );
             },
           ),
