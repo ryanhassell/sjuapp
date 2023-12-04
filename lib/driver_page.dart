6afedaa1cb8e207b69c6d9fead37b2e389742f88
chris <cf751653@sju.edu>
2023-12-04 18:16:47 -0500

    updated driver page and added a new pickup ride page for the driver side

--- lib/driver_page.dart
+++ lib/driver_page.dart
@@ -1,23 +1,32 @@
 import 'package:flutter/material.dart';
-import 'current_ride_page.dart';
+import 'pickup_ride_page.dart'; // Make sure to import your PickupRidePage
 
 class DriverPage extends StatelessWidget {
+  Widget _buildMainButton({required String label, required VoidCallback onPressed}) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      child: Text(label),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Driver Page'),
       ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            //
-            // Navigator.push(
-            //   context,
-            //   MaterialPageRoute(builder: (context) => CurrentRidePage()),
-            // );
-          },
-          child: Text('Current Ride Requests'),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Center(
+          child: _buildMainButton(
+            label: 'Current Ride Requests',
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => PickupRidePage()), // Replace with your PickupRidePage
+              );
+            },
+          ),
         ),
       ),
     );
