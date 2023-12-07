791a8817ef831191b486d5188341944c17668088
erinzahner <ez749184@sju.edu>
2023-12-07 10:50:57 -0500

    added  location button to driver page

--- lib/driver_page.dart
+++ lib/driver_page.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:location/location.dart'; // Import the Location service
 import 'pickup_ride_page.dart';
+import 'location_service.dart'; // Import the LocationService class
+
 
 class DriverPage extends StatelessWidget {
-  Widget _buildMainButton({required String label, required VoidCallback onPressed}) {
+  final LocationService _locationService = LocationService(); // Initialize LocationService
+
+  Widget _buildMainButton(
+      {required String label, required VoidCallback onPressed}) {
     return ElevatedButton(
       onPressed: onPressed,
       child: Text(label),
@@ -18,6 +24,39 @@ class DriverPage extends StatelessWidget {
     );
   }
 
+  Future<void> _enableLocationAccess(BuildContext context) async {
+    LocationData? locationData = await _locationService.getCurrentLocation();
+    if (locationData != null) {
+      // Location access granted, update UI with location data
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Location Data'),
+            content: Text('Latitude: ${locationData.latitude}, Longitude: ${locationData.longitude}'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text('OK'),
+              ),
+            ],
+          );
+        },
+      );
+    } else {
+      // Location access denied or not granted, show error message
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Failed to get location data'),
+          duration: Duration(seconds: 3),
+          behavior: SnackBarBehavior.floating,
+        ),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,14 +68,26 @@ class DriverPage extends StatelessWidget {
         padding: const EdgeInsets.all(16.0),
         child: Align(
           alignment: Alignment.topCenter,
-          child: _buildMainButton(
-            label: 'Current Ride Requests',
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => PickupRidePage()),
-              );
-            },
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              _buildMainButton(
+                label: 'Current Ride Requests',
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => PickupRidePage()),
+                  );
+                },
+              ),
+              SizedBox(height: 20),
+              _buildMainButton(
+                label: 'Allow Location Access',
+                onPressed: () {
+                  _enableLocationAccess(context); // Call function for location access
+                },
+              ),
+            ],
           ),
         ),
       ),
